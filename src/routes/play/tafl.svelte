e83eee5940c57545888821e1c5c553f59bd80aa7
johannesbeere <123079857+johannesbeere@users.noreply.github.com>
2023-02-23 12:55:27 +0000

    ajou des pions et ajout des couleurs

--- src/routes/play/tafl.svelte
+++ src/routes/play/tafl.svelte
@@ -2,15 +2,33 @@
 	import { Vector } from '$lib/utils/datypes';
 
 	const grid: number[][] = [];
-	const whites = [
-		[4,2],
-		[4,3],
-		[4,5],
-		[4,6],
-		[2,4],
-		[3,4],
-		[5,4],
-		[6,4],
+	const peons = [
+		[1, 4,2],
+		[1, 4,3],
+		[1, 4,5],
+		[1, 4,6],
+		[1, 2,4],
+		[1, 3,4],
+		[1, 5,4],
+		[1, 6,4],
+		[2, 3,0],
+		[2, 4,0],
+		[2, 5,0],
+		[2, 4,1],
+		[2, 0,3],
+		[2, 0,4],
+		[2, 0,5],
+		[2, 1,4],
+		[2, 3,8],
+		[2, 4,8],
+		[2, 5,8],
+		[2, 4,7],
+		[2, 8,3],
+		[2, 8,4],
+		[2, 8,5],
+		[2, 7,4],
+		[3, 4,4],
+
 	]
 	for (let y = 0; y < 9; y++) {
 		grid[y] = [];
@@ -18,16 +36,16 @@
 			grid[y][x] = 0;
 		}
 	}
-	for (const pos of whites) {
-		const x = pos[0]
-		const y = pos[1]
-		grid[y][x]=1
+	for (const pos of peons) {
+		const x = pos[1]
+		const y = pos[2]
+		grid[y][x]=pos[0]
 	}
 	let selected: Vector | null = null;
 	function click(x: number, y: number) {
 		if (selected == null && grid[y][x]!=0) {
 			selected = new Vector(x, y);
-		} else {
+		} else if(selected) {
 			if (grid[y][x] == 0 && (selected.y == y || selected.x == x)) {
 				grid[y][x] = grid[selected.y][selected.x];
 				grid[selected.y][selected.x]=0
@@ -44,11 +62,10 @@
 			{#each row as cell, cellX}
 				<td
 					class:selected={selected?.x == cellX && selected?.y == cellY}
+					class="type-{cell}"
 					on:click={() => click(cellX, cellY)}>
 					<!-- {cellX}:{cellY} -->
-					{cell}
-					</td
-				>
+				</td>
 			{/each}
 		</tr>
 	{/each}
@@ -56,23 +73,41 @@
 
 <style>
 	td {
-		width: 50px;
-		height: 50px;
-		border: 1px black solid;
+		/* width: 50px;
+		height: 50px; */
+		box-sizing: border-box;
+		flex: 1;
+		aspect-ratio: 1/1;
+		border: 1px rgb(24, 23, 23) solid;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		background-color: white;
+		/* background-color: grey; */
 		user-select: none;
 	}
 	td.selected {
-		background-color: red;
+		/* border-color: red;
+		border-width: 5px; */
+		box-shadow: inset 0px 0px 0px 5px #f00;
 	}
 	tr {
 		display: flex;
+		/* width: 100%; */
 	}
 	table {
 		border: 1px black solid;
-		width: max-content;
+		/* width: max-content; */
+		width: 90vmin;
+		aspect-ratio: 1/1;
+		margin: auto;
+	}
+	.type-1{
+		background-color: white;
+	}
+	.type-3{
+		background-color: yellow;
+	}
+	.type-2{
+		background-color: black;
 	}
 </style>
